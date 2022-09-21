3848ac1720c20e5c6656d6cfb96a65de0b9185b5
Maurisz <p2002adam@gmail.com>
2022-09-21 14:56:50 +0200

    popup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import './main.css';
+
+function init(){
+
+}
+
+document.addEventListener('DOMContentLoaded',init);
